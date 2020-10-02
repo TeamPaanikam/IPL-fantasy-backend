d4b3f1e6996d63eefc85512208ff368fe1e3df80
Azim Javed <thatazimjaved@gmail.com>
2020-10-02 20:32:19 +0530

    Points fixes

--- libs/points.js
+++ libs/points.js
@@ -7,54 +7,31 @@ const scraper = require('./scraper')
  */
 
 exports.calculate = async (userTeams, scoring) => {
-    /*
-    Flow: userTeams must include user.username, matchPlayer.playerId, matchPlayer.playerName, match.matchUrl. Key elements are matchId and playerId.
-    something like 
 
-    SELECT userTeam.username, matchPlayer.playerId, matchPlayer.playerName, match.matchUrl from user, matchPlayer, match WHERE
-    matchPlayer.playerId = userTeam.playerId AND matchPlayer.matchId = userTeam.matchId AND match.matchId = userTeam.matchId;
-
-    */
-    /*
-     userTeams schema i hope = {
-         matchUrl: matchUrl
-         users: [
-         {username: username, totalScore: 69, team: [{name: playerName, captain: false}]}
-         ]
-     }
-    */
     let pointsTable = {}, multiplier = 1
     let scoreboard = await scraper.cricinfoWorker(userTeams.matchUrl)
-    // console.log(scoreboard)
     userTeams.users.forEach(user => {
-        // console.log(user)
+
         pointsTable[user.username] = {}
         pointsTable[user.username].currentScore = 0
         pointsTable[user.username].totalScore = user.totalScore
         user.team.forEach(playerName => {
-        
-            // console.log(scoreboard.batsmen[playerName.name]);
+    
             if(playerName.captain){
                 multiplier = scoring.captainMultiplier
             } else {
                 multiplier = 1
             }
             if(scoreboard.batsmen[playerName.name] != null){
-                // console.log("here")
-                // console.log(scoreboard.batsmen[playerName.name])
                 pointsTable[user.username].currentScore += multiplier*scoring.run*Number.parseInt(scoreboard.batsmen[playerName.name].runs)
-                // console.log("1 : ", pointsTable[user.username].currentScore,Number.parseInt(scoreboard.batsmen[playerName.name].runs) );
             }
-            if(scoreboard.bowlers[playerName] != null){
+            if(scoreboard.bowlers[playerName.name] != null){
                 pointsTable[user.username].currentScore += multiplier*scoring.wicket*Number.parseInt(scoreboard.bowlers[playerName.name].wickets)
-                // console.log("2 ",  pointsTable[user.username].currentScore, Number.parseInt(scoreboard.bowlers[playerName.name].wickets));
-                
             }
         })
         pointsTable[user.username].totalScore += Number.parseInt(pointsTable[user.username].currentScore)
-        // console.log(user.username, pointsTable[user.username]);
     })
-    // console.log(pointsTable);
-    pointsTable
+    console.log(pointsTable);
+
     return pointsTable
 }
